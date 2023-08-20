81f736ecc81b7a86ee10e0ca16a5612fb1016e91
IgorSulj <igorsuljin@gmail.com>
2023-08-20 10:32:43 +0300

    Added upper part of form

---
+++ src/lib/icons/Icon.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let text: string = ''
+</script>
+
+<div id="wrapper">
+    <div id="wrapper__icon">
+        <div id="icon">
+            <slot />
+        </div>
+    </div>
+    {#if text}
+    <p>{text}</p>
+    {/if}
+</div>
+
+<style>
+    #wrapper__icon {
+        margin: auto;
+        width: 2.5rem;
+        height: 2.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #3081BB;
+        border-radius: 50%;
+    }
+
+    #icon {
+        width: 1.5rem;
+        height: 1.5rem;
+        position: relative;
+    }
+
+    #icon :global(svg *) {
+        fill: white;
+    }
+</style>
---
+++ src/routes/form/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import CheckIcon from "$lib/icons/CheckIcon.svelte";
+</script>
+
+<article>
+    <div class="subform-buttons">
+        <CheckIcon />
+        <CheckIcon />
+        <CheckIcon />
+        <CheckIcon />
+        <CheckIcon />
+        <CheckIcon />
+    </div>
+    <div class="subform-buttons lower">
+        <CheckIcon />
+        <CheckIcon />
+    </div>
+</article>
+
+<style>
+    :global(body) {
+        margin: 0;
+    }
+
+    article {
+        background-color: #eee;
+        width: 100vw;
+        min-height: 100vh;
+    }
+
+    .subform-buttons {
+        display: flex;
+        padding: 15px 10px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px; 
+    }
+
+    .lower {
+        padding: 0;
+    }
+</style>
